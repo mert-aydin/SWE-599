365409df3ddc452e94c21eabd9db3d232edeaed3
Mert Aydın <mertaydin97@gmail.com>
2023-11-21 22:46:09 +0300

    some method turned into functions

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -1,48 +1,45 @@
 import pandas as pd
 
 
-class DataPreprocessor:
-    def __init__(self):
-        pass  # No arguments needed for initialization
+def preprocess_commits(commits):
+    """
+    Preprocess a list of commit dictionaries.
 
-    def preprocess_commits(self, commits):
-        """
-        Preprocess a list of commit dictionaries.
+    :param commits: List of dictionaries, each representing a commit.
+    :return: A DataFrame with preprocessed commit data.
+    """
+    if not commits or not isinstance(commits, list):
+        raise ValueError("Commits should be a list of dictionaries.")
 
-        :param commits: List of dictionaries, each representing a commit.
-        :return: A DataFrame with preprocessed commit data.
-        """
-        if not commits or not isinstance(commits, list):
-            raise ValueError("Commits should be a list of dictionaries.")
+    # Convert the list of dictionaries to a DataFrame
+    commits_df = pd.DataFrame(commits)
 
-        # Convert the list of dictionaries to a DataFrame
-        commits_df = pd.DataFrame(commits)
+    # Example preprocessing steps:
+    # Normalize data, handle missing values, etc.
+    commits_df['commit_date'] = pd.to_datetime(commits_df['commit_date'])
+    commits_df['message'] = commits_df['message'].str.lower().str.replace('[^\w\s]', '', regex=True)
 
-        # Example preprocessing steps:
-        # Normalize data, handle missing values, etc.
-        commits_df['commit_date'] = pd.to_datetime(commits_df['commit_date'])
-        commits_df['message'] = commits_df['message'].str.lower().str.replace('[^\w\s]', '', regex=True)
+    return commits_df
 
-        return commits_df
 
-    def preprocess_issues(self, issues):
-        """
-        Preprocess a list of issue dictionaries.
+def preprocess_issues(issues):
+    """
+    Preprocess a list of issue dictionaries.
 
-        :param issues: List of dictionaries, each representing an issue.
-        :return: A DataFrame with preprocessed issue data.
-        """
-        if not issues or not isinstance(issues, list):
-            raise ValueError("Issues should be a list of dictionaries.")
+    :param issues: List of dictionaries, each representing an issue.
+    :return: A DataFrame with preprocessed issue data.
+    """
+    if not issues or not isinstance(issues, list):
+        raise ValueError("Issues should be a list of dictionaries.")
 
-        # Convert the list of dictionaries to a DataFrame
-        issues_df = pd.DataFrame(issues)
+    # Convert the list of dictionaries to a DataFrame
+    issues_df = pd.DataFrame(issues)
 
-        # Example preprocessing steps:
-        # Normalize data, handle missing values, etc.
-        issues_df['created_at'] = pd.to_datetime(issues_df['created_at'])
-        issues_df['closed_at'] = pd.to_datetime(issues_df['closed_at'])
-        issues_df['title'] = issues_df['title'].str.lower().str.replace('[^\w\s]', '', regex=True)
-        issues_df['body'] = issues_df['body'].str.lower().str.replace('[^\w\s]', '', regex=True)
+    # Example preprocessing steps:
+    # Normalize data, handle missing values, etc.
+    issues_df['created_at'] = pd.to_datetime(issues_df['created_at'])
+    issues_df['closed_at'] = pd.to_datetime(issues_df['closed_at'])
+    issues_df['title'] = issues_df['title'].str.lower().str.replace('[^\w\s]', '', regex=True)
+    issues_df['body'] = issues_df['body'].str.lower().str.replace('[^\w\s]', '', regex=True)
 
-        return issues_df
+    return issues_df
--- visualizer.py
+++ visualizer.py
@@ -2,64 +2,62 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-class Visualizer:
-    @staticmethod
-    def plot_line_chart(data, x, y, title):
-        """
-        Plot a line chart.
+def plot_line_chart(data, x, y, title):
+    """
+    Plot a line chart.
 
-        :param data: DataFrame containing the data to be plotted.
-        :param x: Column name for the x-axis.
-        :param y: Column name for the y-axis.
-        :param title: Title of the plot.
-        """
-        plt.figure(figsize=(10, 6))
-        sns.lineplot(data=data, x=x, y=y)
-        plt.title(title)
-        plt.show()
+    :param data: DataFrame containing the data to be plotted.
+    :param x: Column name for the x-axis.
+    :param y: Column name for the y-axis.
+    :param title: Title of the plot.
+    """
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(data=data, x=x, y=y)
+    plt.title(title)
+    plt.show()
 
-    @staticmethod
-    def plot_bar_chart(data, x, y, title):
-        """
-        Plot a bar chart.
 
-        :param data: DataFrame containing the data to be plotted.
-        :param x: Column name for the x-axis.
-        :param y: Column name for the y-axis.
-        :param title: Title of the plot.
-        """
-        plt.figure(figsize=(10, 6))
-        sns.barplot(data=data, x=x, y=y)
-        plt.title(title)
-        plt.show()
+def plot_bar_chart(data, x, y, title):
+    """
+    Plot a bar chart.
 
-    @staticmethod
-    def plot_histogram(data, column, title, bins=20):
-        """
-        Plot a histogram.
+    :param data: DataFrame containing the data to be plotted.
+    :param x: Column name for the x-axis.
+    :param y: Column name for the y-axis.
+    :param title: Title of the plot.
+    """
+    plt.figure(figsize=(10, 6))
+    sns.barplot(data=data, x=x, y=y)
+    plt.title(title)
+    plt.show()
 
-        :param data: DataFrame containing the data to be plotted.
-        :param column: Column name for which the histogram is to be plotted.
-        :param title: Title of the plot.
-        :param bins: Number of bins in the histogram.
-        """
-        plt.figure(figsize=(10, 6))
-        sns.histplot(data[column], bins=bins)
-        plt.title(title)
-        plt.show()
 
-    @staticmethod
-    def plot_heatmap(data, title, x_label, y_label):
-        """
-        Plot a heatmap.
+def plot_histogram(data, column, title, bins=20):
+    """
+    Plot a histogram.
 
-        :param data: 2D data array for heatmap.
-        :param title: Title of the plot.
-        :param x_label: Label of X-axis.
-        :param y_label: Label of Y-axis.
-        """
-        sns.heatmap(data, annot=True, cmap='coolwarm')
-        plt.title(title)
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
-        plt.show()
+    :param data: DataFrame containing the data to be plotted.
+    :param column: Column name for which the histogram is to be plotted.
+    :param title: Title of the plot.
+    :param bins: Number of bins in the histogram.
+    """
+    plt.figure(figsize=(10, 6))
+    sns.histplot(data[column], bins=bins)
+    plt.title(title)
+    plt.show()
+
+
+def plot_heatmap(data, title, x_label, y_label):
+    """
+    Plot a heatmap.
+
+    :param data: 2D data array for heatmap.
+    :param title: Title of the plot.
+    :param x_label: Label of X-axis.
+    :param y_label: Label of Y-axis.
+    """
+    sns.heatmap(data, annot=True, cmap='coolwarm')
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
